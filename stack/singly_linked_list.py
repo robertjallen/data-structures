8c2268abbb5dec56451094aa7e1d4cbae6a7ffc1
robertjallen <bobbidigi34@gmail.com>
2020-08-20 16:12:33 -0400

    add MVP all tests passing in queue

--- stack/singly_linked_list.py
+++ stack/singly_linked_list.py
@@ -1,30 +1,34 @@
 class Node:
-    def __init__(self, value, next=None):
+    def __init__(self, value=None, next_node=None):
+        # the value at this linked list node
         self.value = value
-        self.next = next  # The next node in the list
-
-
+        # reference to the next node in the list
+        self.next_node = next_node
+    def get_value(self):
+        return self.value
+    def get_next(self):
+        return self.next_node
+    def set_next(self, new_next):
+        # set this node's next_node reference to the passed in node
+        self.next_node = new_next
 class LinkedList:
     def __init__(self):
         self.head: Node = None  # points to the first node in the list
         self.tail: Node = None  # Points to the last node in the list
         self.length = 0
-
     def __str__(self):
         pass
-
     def add_to_head(self, value):
         new_node = Node(value)
         if not self.head and not self.tail:
             self.head = new_node
-            self.tail = new_node 
+            self.tail = new_node
         else:
             new_node.set_next(self.head)
             self.head = new_node
-
     def add_to_tail(self, value):
         # Check if there's a tail
-        # If there is no tail (empty list)    
+        # If there is no tail (empty list)
         if self.tail is None:
             # Create a new node
             new_tail = Node(value, None)
@@ -37,11 +41,10 @@ class LinkedList:
             new_tail = Node(value, None)
             # Set current tail.next to the new node
             old_tail = self.tail
-            old_tail.next = new_tail
+            old_tail.next_node = new_tail
             # Set self.tail to the new node
             self.tail = new_tail
         self.length += 1
-
     def remove_head(self):
         # If not head (empty list)
         if self.head is None:  # if self.head is None
@@ -60,33 +63,25 @@ class LinkedList:
         else:
             # 	Set self.head to current_head.next
             current_head = self.head
-            self.head = current_head.next
+            self.head = current_head.next_node
             #  Return current_head.value
             self.length = self.length - 1
             return current_head.value
-
     def remove_tail(self):
-        # Remove Tail:
-        # Check if it's there
-        if not self.tail:
+        # first if you donnt have head (aka no linked list exists),
+        if not self.head:
             return None
-        # List of 1 element:
-        if self.tail == self.head:
-            # Save the current_tail.value
-            current_tail = self.tail
-            # Set self.tail to None
-            # Set self.head to None
-            self.tail = None
+        # if linked list has 1 node
+        if self.head is self.tail:
+            value = self.head.get_value()
             self.head = None
-            self.length -= 1
-            return current_tail.value
-        # General case:
-        # Start at head and iterate to the next-to-last node
-        else:
-            while current.node.next != self.tail:
-                current.node.next
-
-        # Stop when current_node.next == self.tail
-        # Save the current_tail value
-        # Set self.tail to current_node
-        # Set current_node.next to None
+            self.tail = None
+            return value
+        current = self.head
+        while current.get_next() is not self.tail:
+            current = current.get_next()  # goes through entire list until next node is tail
+        # save value of tail beause that is what we will return later
+        value = self.tail.get_value()
+        self.tail = current  # now tail is also current
+        self.tail.set_next(None)
+        return value
